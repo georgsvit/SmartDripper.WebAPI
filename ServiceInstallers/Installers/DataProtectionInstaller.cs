b22bfe02599dd0baf20eaf90f1098da9cb0db30b
George Svitenko <georg.svit@gmail.com>
2020-11-11 16:40:03 +0200

    Data protection service was added.

---
+++ ServiceInstallers/Installers/DataProtectionInstaller.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SmartDripper.WebAPI.ServiceInstallations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartDripper.WebAPI.ServiceInstallers.Installers
+{
+    public class DataProtectionInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDataProtection();
+        }
+    }
+}
