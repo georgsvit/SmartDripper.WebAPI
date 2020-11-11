af273e29b4271b2ffea249503e1fb9689bc55227
George Svitenko <georg.svit@gmail.com>
2020-11-11 16:51:40 +0200

    Data protection was added to GenericUserService.

--- Services/GenericUserService.cs
+++ Services/GenericUserService.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.EntityFrameworkCore;
+using SmartDripper.WebAPI.Contracts.DTORequests;
 using SmartDripper.WebAPI.Data;
 using SmartDripper.WebAPI.Models.Users;
 using System;
@@ -10,11 +12,13 @@ namespace SmartDripper.WebAPI.Services
     {
         protected readonly ApplicationContext applicationContext;
         protected readonly JWTTokenService tokenService;
+        protected readonly IDataProtector dataProtector;
 
-        protected GenericUserService(ApplicationContext applicationContext, JWTTokenService tokenService)
+        protected GenericUserService(ApplicationContext applicationContext, JWTTokenService tokenService, IDataProtector dataProtector)
         {
             this.applicationContext = applicationContext;
             this.tokenService = tokenService;
+            this.dataProtector = dataProtector;
         }
 
         public async Task RegisterUserAsync<TUser>(TUser user) where TUser : User
@@ -24,24 +28,35 @@ namespace SmartDripper.WebAPI.Services
                 throw new Exception("The user with such login is already registered.");
             }
 
-            //SecurePassword(user);
+            ProtectPassword(user);
             await applicationContext.Set<TUser>().AddAsync(user);
             await applicationContext.SaveChangesAsync();
         }
 
-        //protected async Task<UserIdentity> GetUserIdentityAsync()
+        protected async Task<UserIdentity> GetIdentityAsync(LoginRequest request)
+        {
+            var identity = await GetIdentityByLoginAsync(request.Login);
 
-        //private void SecurePassword<TUser>(TUser user) where TUser : User =>
-        //    user.UserIdentity.Password = passwordService.GeneratePassword(user.UserIdentity.Password);
+            if (identity == null) throw new Exception("The user with such login doesn`t exist.");
+
+            var invalidPassword = dataProtector.Unprotect(identity.Password) != request.Password;
+
+            if (invalidPassword) throw new Exception("The password inn`t correct");
+
+            return identity;
+        }
+
+        private void ProtectPassword<TUser>(TUser user) where TUser : User =>
+            user.UserIdentity.Password = dataProtector.Protect(user.UserIdentity.Password);
 
         private async Task<bool> IsUserRegisteredAsync<TUser>(TUser user) where TUser : User
         {
-            UserIdentity registeredIdentity = await GetUserIdentityByLoginAsync(user.UserIdentity.Login);
+            UserIdentity registeredIdentity = await GetIdentityByLoginAsync(user.UserIdentity.Login);
             bool isAlreadyRegistered = registeredIdentity != null;
             return isAlreadyRegistered;
         }
 
-        private async Task<UserIdentity> GetUserIdentityByLoginAsync(string login) =>
+        private async Task<UserIdentity> GetIdentityByLoginAsync(string login) =>
             await applicationContext.UserIdentities.FirstOrDefaultAsync(identity => identity.Login == login);
     }
 }
