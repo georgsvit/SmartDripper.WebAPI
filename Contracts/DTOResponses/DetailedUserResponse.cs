fd86caa0b427dc1bd1433c3ba9470c0fb3b95cc0
George Svitenko <georg.svit@gmail.com>
2020-11-11 16:59:51 +0200

    Responses for DetailedUser and JWTToken were added.

---
+++ Contracts/DTOResponses/DetailedUserResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartDripper.WebAPI.Contracts.DTOResponses
+{
+    public class DetailedUserResponse : JWTTokenResponse
+    {
+        public DetailedUserResponse(string name, string surname, string role, string token, DateTime expireDate) 
+            : base(token, expireDate)
+        {
+            Name = name;
+            Surname = surname;
+            Role = role;
+        }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Role { get; set; }
+    }
+}
---
+++ Contracts/DTOResponses/JWTTokenResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartDripper.WebAPI.Contracts.DTOResponses
+{
+    public class JWTTokenResponse
+    {
+        public JWTTokenResponse(string token, DateTime expireDate)
+        {
+            Token = token;
+            ExpireDate = expireDate;
+        }
+
+        public string Token { get; set; }
+        public DateTime ExpireDate { get; set; }
+    }
+}
