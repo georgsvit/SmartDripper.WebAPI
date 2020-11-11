331e200150947379430f3817a72745aec39571e6
George Svitenko <georg.svit@gmail.com>
2020-11-11 17:14:33 +0200

    AdminService was provided.

--- Services/AdminService.cs
+++ Services/AdminService.cs
@@ -1,13 +1,28 @@
-﻿using SmartDripper.WebAPI.Data;
+﻿using Microsoft.AspNetCore.DataProtection;
+using SmartDripper.WebAPI.Contracts.DTORequests;
+using SmartDripper.WebAPI.Contracts.DTOResponses;
+using SmartDripper.WebAPI.Data;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Threading.Tasks;
 
 namespace SmartDripper.WebAPI.Services
 {
     public class AdminService : GenericUserService
     {
-        public AdminService(ApplicationContext applicationContext,
-                            JWTTokenService tokenService)
-            : base(applicationContext, tokenService)
-        { }
+        public AdminService(ApplicationContext applicationContext, JWTTokenService tokenService, IDataProtector dataProtector)
+            : base(applicationContext, tokenService, dataProtector) { }
 
+        public async Task<DetailedUserResponse> LoginAsync(LoginRequest loginRequest)
+        {
+            var identity = await GetIdentityAsync(loginRequest);
+            var user = await applicationContext.Admin.FindAsync(identity.Id);
+            if (user == null) throw new Exception("Login failed. The user is not an admin.");
+
+            JwtSecurityToken token = tokenService.CreateJWTToken(identity);
+            string encodedToken = tokenService.EncodeJWTToken(token);
+
+            return new DetailedUserResponse(user.Name, user.Surname, identity.Role, encodedToken, token.ValidTo);
+        }
     }
 }
